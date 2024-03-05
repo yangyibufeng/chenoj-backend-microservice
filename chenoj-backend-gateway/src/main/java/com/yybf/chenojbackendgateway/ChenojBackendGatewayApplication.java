6afaaf1a47176d23718c3c70566d75ac689330a0
YangYi <1134456853@qq.com>
2024-03-05 22:17:18 +0800

    通过Gateway实现微服务网关，完成了接口路由的配置

--- chenoj-backend-gateway/src/main/java/com/yybf/chenojbackendgateway/ChenojBackendGatewayApplication.java
+++ chenoj-backend-gateway/src/main/java/com/yybf/chenojbackendgateway/ChenojBackendGatewayApplication.java
@@ -2,9 +2,10 @@ package com.yybf.chenojbackendgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-@SpringBootApplication
+// 因为引入的有mybatisplus的依赖，所以启动的时候默认开启一个数据库，会报错，所以直接排除掉数据源自动配置
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 public class ChenojBackendGatewayApplication {
 
