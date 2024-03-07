deaa6c78c6473386b81cd03ff42cff51c4bb5d01
YangYi <1134456853@qq.com>
2024-03-07 21:23:48 +0800

    修复了因为有多个消息队列监听器导致的判题服务无法获取消息队列中的全部消息

--- chenoj-backend-judge-service/src/main/java/com/yybf/chenojbackendjudgeservice/ChenojBackendJudgeServiceApplication.java
+++ chenoj-backend-judge-service/src/main/java/com/yybf/chenojbackendjudgeservice/ChenojBackendJudgeServiceApplication.java
@@ -1,5 +1,6 @@
 package com.yybf.chenojbackendjudgeservice;
 
+import com.yybf.chenojbackendjudgeservice.RabbitMQ.InitRabbitMQ;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
--- chenoj-backend-judge-service/src/main/java/com/yybf/chenojbackendjudgeservice/RabbitMQ/InitRabbitMQ.java
+++ chenoj-backend-judge-service/src/main/java/com/yybf/chenojbackendjudgeservice/RabbitMQ/InitRabbitMQ.java
@@ -1,25 +1,42 @@
 package com.yybf.chenojbackendjudgeservice.RabbitMQ;
 
 import com.rabbitmq.client.Channel;
-import lombok.SneakyThrows;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.support.AmqpHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.stereotype.Component;
 
 /**
+ * 用于创建测试程序用到的交换机和队列（只用在程序启动前执行一次）
+ *
  * @author yangyibufeng
  * @date 2024/3/6
  */
 @Slf4j
-@Component
 public class InitRabbitMQ {
-    // 指定程序监听的消息队列和确认机制
-    @SneakyThrows
-    @RabbitListener(queues = {"code_queue"}, ackMode = "MANUAL")
-    public void receiveMessage(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag) {
-        log.info("receiveMessage message = {}", message);
-        channel.basicAck(deliveryTag, false);
+
+    public static void doInit() {
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            // 和消息队列建立连接
+            Connection connection = factory.newConnection();
+            // 一个操作消息队列的客户端，类似JDBC，RedisClient，DockerClient
+            Channel channel = connection.createChannel();
+            String EXCHANGE_NAME = "code_exchange";
+            channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+
+            // 创建队列，随机分配一个队列名称
+            String queueName = "code_queue";
+            channel.queueDeclare(queueName, true, false, false, null);
+            channel.queueBind(queueName, EXCHANGE_NAME, "my_routingKey");
+            log.info("消息队列启动成功");
+        } catch (Exception e) {
+            log.error("消息队列启动失败" + e.getMessage());
+        }
     }
+
+    public static void main(String[] args) {
+        doInit();
+    }
+
 }
